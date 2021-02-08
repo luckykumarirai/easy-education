155dfa18a5a7dc5cf2a69ac09884a4df424c32cb
Deepak8717 <sendit2deepak@gmail.com>
2021-02-08 18:33:03 +0530

    adds footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-import NavBar from './components/navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <Footer />
     </div>
   );
 }
