d8f4478ab488919645f7866ab259df46eccb555a
soumya-badkul <badkulsoumya@gmail.com>
2021-02-16 22:40:53 +0530

    added contactpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./components/navbar";
 import Footer from "./components/Footer";
 import {Home} from "./components/home";
+import {Contact} from "./components/Contact";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 
@@ -11,10 +12,10 @@ function App() {
     <div className="App">
       <NavBar />
     <Router>
-       <Switch>
-		      <Route exact path="/home" component={Home}/>
-          <Link to='/home'>Go to Home Page</Link>
-	    </Switch>
+      <Switch>
+      <Route exact path="/contact" component={Contact}/>
+          <Link to='/contact'>Go to Contact Page</Link>
+      </Switch>
     </Router>
       {/* <Footer /> */}
     </div>
