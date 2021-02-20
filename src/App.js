dd4661d6bff550661f17ac4294ca2bb57effcd1c
soumya-badkul <badkulsoumya@gmail.com>
2021-02-20 14:56:39 +0530

    added privacy-policy-page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signin from "./components/Signin";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Aboutus from "./components/Aboutus";
 import TermsAndConditions from "./components/TermsAndConditions";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 function App() {
   return (
@@ -20,9 +21,10 @@ function App() {
         <Route exact path="/home" component={Home}/>
         <Route path="/Aboutus" exact component={Aboutus} />
         <Route path="/Terms-and-Conditions" exact component={TermsAndConditions} />
+        <Route path="/Privacy-policy" exact component={PrivacyPolicy} />
       </Switch>
     </Router>
-    <Footer />
+    {/* <Footer /> */}
     </div>
   );
 }
